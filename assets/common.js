07ea5589e3f5b1899c4fb3d925913f8bbc2eea28
sreesridharp <sridharp@wekancode.com>
2021-06-09 11:20:02 +0530

    added privacy policy and terms pages. added link to social mediaicons on index footer page.

--- assets/common.js
+++ assets/common.js
@@ -1,36 +1,67 @@
-(function(){
-    function id(v){return document.getElementById(v); }
-    function loadbar() {
-      var ovrl = id("overlay"),
-          prog = id("progress"),
-          stat = id("progstat"),
-          img = document.images,
-          c = 0;
-          tot = img.length;
-  
-      function imgLoaded(){
-        c += 1;
-        var perc = ((100/tot*c) << 0) +"%";
-        prog.style.width = perc;
-        stat.innerHTML = "Loading "+ perc;
-        if(c===tot) return doneLoading();
-      }
-      function doneLoading(){
-        ovrl.style.opacity = 0;
-        setTimeout(function(){ 
-          ovrl.style.display = "none";
-        }, 1200);
-      }
-      for(var i=0; i<tot; i++) {
-        var tImg     = new Image();
-        tImg.onload  = imgLoaded;
-        tImg.onerror = imgLoaded;
-        tImg.src     = img[i].src;
-      }    
-    }
-    document.addEventListener('DOMContentLoaded', loadbar, false);
+(function () {
+  function id(v) {
+    return document.getElementById(v);
+  }
+  function loadbar() {
+    var ovrl = id("overlay"),
+      prog = id("progress"),
+      stat = id("progstat"),
+      img = document.images,
+      c = 0;
+    tot = img.length;
 
-    const getNewDate = new Date();
-    const getCurrentYear = getNewDate.getFullYear();
-    document.getElementById("currentYear").innerHTML = getCurrentYear
-  }());
+    function imgLoaded() {
+      c += 1;
+      var perc = (((100 / tot) * c) << 0) + "%";
+      prog.style.width = perc;
+      stat.innerHTML = "Loading " + perc;
+      if (c === tot) return doneLoading();
+    }
+    function doneLoading() {
+      ovrl.style.opacity = 0;
+      setTimeout(function () {
+        ovrl.style.display = "none";
+      }, 1200);
+    }
+    for (var i = 0; i < tot; i++) {
+      var tImg = new Image();
+      tImg.onload = imgLoaded;
+      tImg.onerror = imgLoaded;
+      tImg.src = img[i].src;
+    }
+  }
+  document.addEventListener("DOMContentLoaded", loadbar, false);
+})();
+
+
+  // Get current year for copyrights
+  const getNewDate = new Date();
+  const getCurrentYear = getNewDate.getFullYear();
+  const appendYear = document.getElementById("currentYear");
+  if(appendYear) {
+    appendYear.innerHTML = getCurrentYear;
+  }
+
+  // Scroll to top
+
+  //Get the button
+  var mybutton = document.getElementById("btn-scroll-top");
+  
+  // When the user scrolls down 20px from the top of the document, show the button
+  window.onscroll = function() {scrollFunction()};
+  
+  function scrollFunction() {
+    if (mybutton) {
+      if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+      } else {
+        mybutton.style.display = "none";
+      }
+    }
+  }
+  
+  // When the user clicks on the button, scroll to the top of the document
+  function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollIntoView({ behavior: 'smooth' });
+  }
